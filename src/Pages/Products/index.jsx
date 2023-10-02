0da811a5cc5f5011b65888f52bedeb841a36ce54
Dulce Jaramillo <dulcejaramillo@dulces-MacBook-Pro.local>
2023-10-01 21:18:14 -0700

    added use effect

--- src/Pages/Products/index.jsx
+++ src/Pages/Products/index.jsx
@@ -1,22 +1,49 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 const Products = () => {
     
     const[products, setProducts]= useState(["milk","bread","eggs","cheese"])
-    const[name,setName]= useState()
-    const[description, setDescription]= useState()
-    const[price,setPrice]= useState()
+    const[name,setName]= useState("")
+    const[description, setDescription]= useState("")
+    const[price,setPrice]= useState("")
+    const[newName,setNewName]= useState("")
+    const[newDescription, setNewDescription]= useState("")
+    const[newPrice,setNewPrice]= useState("")
  
- 
- 
+ const addProduct =(e) => {
+    e.preventDefault()
+    setNewName(name)
+    setNewDescription(description)
+    setNewPrice(price)
+
+
+    class Product{
+        constructor(name, description, price) {
+        this.name= name;
+        this.description= description; 
+        this.price= price;
+       }}
+       const product= new Product(name,description,price)
+        return product
+   
+products.push(product)
+
+useEffect(()=>{
+console.log("A new product has been added!", {products})
+},[])
+
+setName('')
+setDescription("")
+setPrice("")
+ }
     return (
     <>
     <h1>Products</h1>
 
-    <input value={name} onChange={()=> setName(e.target.value)}></input>
-    <input value={description} onChange={()=> setDescription(e.target.value)}></input>
-    <input value={price} onChange={()=> setPrice(e.target.value)}></input>
-    <button onClick={}>Submit</button>
+    <input value={name} placeholder="Name" onChange={(e)=> setName(e.target.value)}/>
+    <input value={description} placeholder="Description"onChange={(e)=> setDescription(e.target.value)}/>
+    <input value={price} placeholder="Price" onChange={(e)=> setPrice(e.target.value)}/>
+    <button onClick={addProduct}>Add Product</button>
     </>
   )
 }
